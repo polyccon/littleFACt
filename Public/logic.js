9d65870609745f6794efebcc017e63a0529d4efc
ameliejyc <amelie.chan@gmail.com>
2017-07-12 17:31:04 +0100

    initial wikipedia api call relates #12

--- Public/logic.js
+++ Public/logic.js
@@ -1,14 +1,45 @@
+// var api = {}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// if (typeof module !== 'undefined') {
+//   module.exports = api;
+// }
 
-var api = {  }
+(function() {
+  var destString = "Bethnal%20Green";
+  var wikiURL = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=" + destString;
 
+  function wikiExtract() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
 
+        var data = JSON.parse(xhr.responseText);
+        var keys = Object.keys(data.query.pages);
+        var key = Number(keys[0]);
+        console.log(key);
+        var destinationDrop = document.getElementById("destination");
+        var destinationName = data.query.pages;
+        destinationDrop.textContent = destinationName;
 
+        var extractDrop = document.getElementById("destination-extract");
+        var extract = data.extract;
+        extractDrop.textContent = extract;
+        // var name = data.name;
+        // nameDrop.textContent = name;
+        console.log(destinationName);
+      }
+    }
 
-
-
-
-
-
-if (typeof module !== 'undefined') {
-  module.exports = api;
-}
+    xhr.open("GET", wikiURL, true);
+    xhr.send();
+  }
+  wikiExtract();
+})();

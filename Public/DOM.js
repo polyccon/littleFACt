db308955a47da4bd8d800351264f2a20bf82572c
Constantina <polyccon@gmail.com>
2017-07-13 17:30:08 +0100

    added tests for string function
    changed logic to make it testable

--- Public/DOM.js
+++ Public/DOM.js
@@ -1,41 +1,39 @@
+(function() {
 
-
-(function( ){
-
-  document.getElementById("from-to").addEventListener("submit",function(event) {
+  document.getElementById("from-to").addEventListener("submit", function(event) {
     event.preventDefault();
-    var from =document.getElementById("from-to")[0].value
-    var inputTo =  document.getElementById("from-to")[1].value
-    var to = document.getElementById("from-to")[1].value ;
+    var from = document.getElementById("from-to")[0].value
+    var inputTo = document.getElementById("from-to")[1].value
+    var to = document.getElementById("from-to")[1].value;
 
-    if (from.indexOf('station') === -1 ) {
-      from += "station" ;
+    if (from.indexOf('station') === -1) {
+      from += "station";
     }
-    if (to.indexOf('station') === -1 ) {
-       to += "station" ;
+    if (to.indexOf('station') === -1) {
+      to += "station";
     }
     // if (inputTo.indexOf('station') !== -1 ) {
     //    inputTo.replace(' station', "");
     // }
     // console.log(inputTo);
-    tflAPI(from,to, function (resultsToShow) {
+    tflAPI(from, to, function(resultsToShow) {
 
 
-       document.getElementById('duration').textContent = resultsToShow.journeys[0].duration;
+      document.getElementById('duration').textContent = resultsToShow.journeys[0].duration;
 
-var lineLeg = resultsToShow.journeys[0].legs;
-console.log(lineLeg);
- lineLeg.forEach(function(line){
-   console.log(line.routeOptions[0].name);
-  var newline = document.createElement('p');
-  newline.textContent = line.routeOptions[0].name ;
-  document.getElementById("lines").appendChild(newline);
+      var lineLeg = resultsToShow.journeys[0].legs;
+      console.log(lineLeg);
+      lineLeg.forEach(function(line) {
+        console.log(line.routeOptions[0].name);
+        var newline = document.createElement('p');
+        newline.textContent = line.routeOptions[0].name;
+        document.getElementById("lines").appendChild(newline);
 
- })
+      })
 
-    } )
+    })
 
-  wikiApi(inputTo);
+    wikiApi(inputTo);
   })
 
 

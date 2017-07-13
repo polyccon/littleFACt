0d8232669576d7e3a745e1aa65b6342cb536dc83
Constantina <polyccon@gmail.com>
2017-07-13 12:26:38 +0100

    wiki extract function and initial tests

--- Public/logic.js
+++ Public/logic.js
@@ -16,11 +16,14 @@
   var to = document.getElementById("from-to")[1].value;
 
   var array = to.toLowerCase().split(" ");
-  var arr = array.map(function(x){
-    return x.slice(0,1).toUpperCase()+x.slice(1);
+  var arr = array.map(function(x) {
+    return x.slice(0, 1).toUpperCase() + x.slice(1);
   });
   var destString = arr.join("%20");
 
+  if (destString === "Angel" || destString=== "Bank" || destString === "Borough" || destString == "Barbican" || destString === "Monument" || destString === "Oval" || destString === "Wimbledon" || destString === "Temple") {
+    destString = destString + ",%20London";
+  }
   var wikiURL = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=" + destString;
 
   function wikiExtract() {
@@ -37,6 +40,17 @@
         var extractDrop = document.getElementById("destination-extract");
         var extr = data.query.pages[keys[0]].extract.replace(/(&nbsp;|<([^>]+)>)/ig, "");
         extractDrop.textContent = extr;
+
+        if (extr.length < 50) {
+          destString = arr.join("%20") + ",%20London";
+          wikiURL = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=" + destString;
+          wikiExtract();
+          console.log(destinationName);
+          console.log(data);
+          if (extr === "") {
+            extractDrop.textContent = "Sorry! " + destinationName + " doesn't seem to have a wikipedia page yet, why don't you make one yourself?"
+          }
+        }
         // var name = data.name;
         // nameDrop.textContent = name;
         console.log(destinationName);
@@ -85,6 +99,7 @@ function tflAPI(from, to) {
 
 if (typeof module !== 'undefined') {
   module.exports = tflAPI;
+  module.exports = wikiExtract;
 }
 
 // westminsterstation/to/bankstation?app_id={{11944170}}&app_key={{b5950c6792c4a2e09bb2331e499ff205}}

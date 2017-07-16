8bc1920f985c75f927adc3e5123a97a3ce4fe324
Constantina <polyccon@gmail.com>
2017-07-16 17:07:29 +0100

    Code refactored
    Error handling not yet added

--- Public/DOM.js
+++ Public/DOM.js
@@ -1,5 +1,3 @@
-
-
 (function() {
 
   document.getElementById("from-to").addEventListener("submit", function(event) {
@@ -14,35 +12,30 @@
     if (to.indexOf('station') === -1) {
       to += "station";
     }
+    httpRequest(tflURL(from, to), tflAPI)
 
-    tflAPI(tflURL(from, to), function(resultsToShow) {
-
-    //  if (resultsToShow.journeys[0].duration){
-      document.getElementById('duration').textContent = resultsToShow.journeys[0].duration;
-      document.getElementById('tfl-results').style.display = 'block';
-    // }else{
-    //   document.getElementById("duration").textContent = "Oops! Station name is not recognised!";
-    //
-    // }
-
-      var lineLeg = resultsToShow.journeys[0].legs;
-      console.log(lineLeg);
-      var linesNode = document.getElementById('lines');
-      linesNode.innerHTML = "";
-      lineLeg.forEach(function(line) {
-        var newline = document.createElement('p');
-        newline.textContent = line.routeOptions[0].name;
-        linesNode.appendChild(newline);
-
-      })
-
-    })
-var wikiURL = createwikiUrl(inputTo);
-httpRequest(wikiURL, wikiExtract);
-
+    var wikiURL = createwikiUrl(inputTo);
+    httpRequest(wikiURL, wikiExtract);
   })
 
 })()
+
+
+function domtflFunction(resultsToShow) {
+
+  document.getElementById('duration').textContent = resultsToShow.journeys[0].duration;
+  document.getElementById('tfl-results').style.display = 'block';
+
+  var lineLeg = resultsToShow.journeys[0].legs;
+  var linesNode = document.getElementById('lines');
+  linesNode.innerHTML = "";
+  lineLeg.forEach(function(line) {
+    var newline = document.createElement('p');
+    newline.textContent = line.routeOptions[0].name;
+    linesNode.appendChild(newline);
+  })
+}
+
 
 
 function domwikiFunction(destinationName, extr) {
@@ -51,4 +44,5 @@ function domwikiFunction(destinationName, extr) {
 
   var extractDrop = document.getElementById("destination-extract");
   extractDrop.textContent = extr;
+  document.getElementById('fun-fact').style.display = 'block';
 }
--- Public/logic.js
+++ Public/logic.js
@@ -1,11 +1,9 @@
-// var testwikiurl = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=victoria,%20London"
-
 function httpRequest(url, nextfunction) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
-     var data =  JSON.parse(xhr.responseText);
-     nextfunction(data);
+      var data = JSON.parse(xhr.responseText);
+      nextfunction(data);
     }
   }
   xhr.open('GET', url, true);
@@ -30,92 +28,55 @@ function createwikiUrl(inputTo) {
 }
 
 
-
 function wikiExtract(data) {
 
- var keys = Object.keys(data.query.pages);
- var destinationName = data.query.pages[keys[0]].title;
- var extr = data.query.pages[keys[0]].extract.replace(/(&nbsp;|<([^>]+)>)/ig, "");
+    var keys = Object.keys(data.query.pages);
+    var destinationName = data.query.pages[keys[0]].title;
+    var extr = data.query.pages[keys[0]].extract.replace(/(&nbsp;|<([^>]+)>)/ig, "");
 
- domwikiFunction(destinationName, extr);
+    if (extr.length < 50) {
+      wikiURL += ",%20London";
+      //wikiExtract();
 
- if (extr.length < 50) {
-   wikiURL += ",%20London";
-   wikiExtract();
-
-   if (extr === "") {
-     extractDrop.textContent = "Sorry! " + destinationName + " doesn't seem to have a wikipedia page yet, why don't you make one yourself?"
-   }
-}
-document.getElementById('fun-fact').style.display = 'block';
-}
-
-
-
-
-  /*function to build TFL Api URL*/
-  function tflURL(from, to) {
-    var TFL_API = "https://api.tfl.gov.uk/journey/journeyresults/"
-    var TFL_key = "?app_id=11944170&app_key=b5950c6792c4a2e09bb2331e499ff205"
-    var url = TFL_API + from + "/to/" + to + TFL_key;
-
-    return url;
-  }
-
-
-
-
-
-  function tflAPI(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-
-      if (xhr.readyState == 4 && xhr.status == 300) {
-        var responseObj = JSON.parse(xhr.responseText);
-
-        var fromOptions = responseObj.fromLocationDisambiguation.disambiguationOptions;
-
-        var toOptions = responseObj.toLocationDisambiguation.disambiguationOptions;
-
-        var matchqufrom = fromOptions.matchQuality;
-
-        var matchquto = toOptions.matchQuality;
-
-
-        //  if (fromOptions !=undefined && toOptions!=undefined && matchquto != undefined && matchqufrom != undefined && matchquto>=855 && matchqufrom >= 855) {
-        var newFrom = fromOptions[0].place.icsCode;
-
-        var newTo = toOptions[0].place.icsCode;
-
-        //GRAB ICS CODE AND PASS ONTO RENDER
-        tflAPI(tflURL(newFrom, newTo), callback);
-        //    } else {
-
-        // console.log(document.getElementById("duration").innerHTML);
-        // var duration = document.getElementById("duration");
-        // duration.innerHTML = "Oops! Station name is not recognised!";
-
-        //alert("Oops! Station name is not recognised!");
-        //console.log(document.getElementById("duration").innerHTML);
-        //   console.log("IM here");
-        // }
-
-      }
-
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        var responseObj200 = JSON.parse(xhr.responseText);
-
-        callback(responseObj200);
+      if (extr === "") {
+        extractDrop.textContent = "Sorry! " + destinationName + " doesn't seem to have a wikipedia page yet, why don't you make one yourself?"
       }
     }
-    xhr.open("GET", url, true);
-    xhr.send();
+    domwikiFunction(destinationName, extr);
+}
+
+
+
+
+/*function to build TFL Api URL*/
+function tflURL(from, to) {
+  var TFL_API = "https://api.tfl.gov.uk/journey/journeyresults/"
+  var TFL_key = "?app_id=11944170&app_key=b5950c6792c4a2e09bb2331e499ff205"
+  var url = TFL_API + from + "/to/" + to + TFL_key;
+
+  return url;
+}
+
+function tflAPI(data) {
+
+  var fromOptions = data.fromLocationDisambiguation.disambiguationOptions;
+  var toOptions = data.toLocationDisambiguation.disambiguationOptions;
+
+  var matchqufrom = fromOptions.matchQuality;
+  var matchquto = toOptions.matchQuality;
+
+
+    var newFrom = fromOptions[0].place.icsCode;
+    var newTo = toOptions[0].place.icsCode;
+
+    domtflFunction(resultsToShow);
+}
+
+
+
+if (typeof module !== 'undefined') {
+  module.exports = {
+    tflURL,
+    destStr
   };
-
-
-  if (typeof module !== 'undefined') {
-    module.exports = {
-      tflURL,
-      destStr
-    };
-  }
+}
